a1ee39b4c3590ddd66744f9142d5cd554b5ba53d
jeff3647 <qkrghdrms123@gmail.com>
2018-08-23 12:08:52 -0500

    hack01
    I love water.

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: X&J
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello I'm Xin Yi. I'm CompSci major.\n");
+  printf("Hello I'm Jeffrey Park. I'm CompSci major.\n");
+
+  return 0;
+}
